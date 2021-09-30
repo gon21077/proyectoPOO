f2864c2c434cfd5035077f48ef61d04db952b2d8
gon21077 <gon21077@uvg.edu.gt>
2021-09-29 18:47:27 -0600

    Logica inicial, preguntas creadas
    Se construyo la clase preguntas, que genera preguntas randomizadas y se agrego al main una logica inicial que corre el programa.

--- main.java
+++ main.java
@@ -2,5 +2,47 @@ import java.util.Scanner;
 
 
 public class main {
-    
+    public static preguntas pregunta = null;
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int vidas = 3;
+
+        while (vidas != 0){
+            pregunta = new preguntas();
+            if (pregunta.getTipo() == "Suma"){
+                boolean R = pregunta.Suma();
+                if (R == true){
+                    System.out.println("Bien hecho!");
+                } else {
+                    System.out.println("Incorrecto, sigue tratando!");
+                    vidas = vidas-1;
+                }
+            } if (pregunta.getTipo()== "Resta"){
+                boolean R = pregunta.Resta();
+                if (R == true){
+                    System.out.println("Bien hecho!");
+                } else {
+                    System.out.println("Incorrecto, sigue tratando!");
+                    vidas = vidas-1;
+                }
+            } if (pregunta.getTipo()== "Multiplicacion"){
+                boolean R = pregunta.Multiplicacion();
+                if (R == true){
+                    System.out.println("Bien hecho!");
+                } else {
+                    System.out.println("Incorrecto, sigue tratando!");
+                    vidas = vidas-1;
+                }
+            } if (pregunta.getTipo()== "Division"){
+                boolean R = pregunta.Division();
+                if (R == true){
+                    System.out.println("Bien hecho!");
+                } else {
+                    System.out.println("Incorrecto, sigue tratando!");
+                    vidas = vidas-1;
+                }
+            }
+        }
+    }
 }
--- preguntas.java
+++ preguntas.java
@@ -1,5 +1,78 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class preguntas {
-    
+    int RespuestaC;
+    int Respuesta;
+    int primerN;
+    int SegundoN;
+    String tipo;
+    ArrayList<String> tipos = new ArrayList<String>();
+    Random random = new Random();
+    Scanner scan = new Scanner(System.in);
+
+    public preguntas(){
+        primerN = (int)(Math.random()*(100));
+        SegundoN = (int)(Math.random()*(100));
+        Collections.addAll(tipos, "Suma", "Resta", "Multiplicacion", "Division");
+        tipo = tipos.get(random.nextInt(tipos.size()));
+    }
+
+    public boolean Suma(){
+        RespuestaC = primerN + SegundoN;
+        System.out.println("Pregunta:");
+        System.out.println(Integer.toString(primerN)+" + "+Integer.toString(SegundoN));
+        System.out.println("Ingrese su respuesta:");
+        Respuesta = scan.nextInt();
+
+        if (Respuesta == RespuestaC){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean Resta(){
+        RespuestaC = primerN - SegundoN;
+        System.out.println("Pregunta:");
+        System.out.println(Integer.toString(primerN)+" - "+Integer.toString(SegundoN));
+        System.out.println("Ingrese su respuesta:");
+        Respuesta = scan.nextInt();
+
+        if (Respuesta == RespuestaC){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean Multiplicacion(){
+        RespuestaC = primerN * SegundoN;
+        System.out.println("Pregunta:");
+        System.out.println(Integer.toString(primerN)+" * "+Integer.toString(SegundoN));
+        System.out.println("Ingrese su respuesta:");
+        Respuesta = scan.nextInt();
+
+        if (Respuesta == RespuestaC){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean Division(){
+        double RespuestaC = primerN/SegundoN;
+        System.out.println("Pregunta:");
+        System.out.println(Integer.toString(primerN)+" - "+Integer.toString(SegundoN));
+        System.out.println("Ingrese su respuesta:");
+        double Respuesta = scan.nextDouble();
+
+        if (Respuesta == RespuestaC){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public String getTipo(){
+        return tipo;
+    }
 }

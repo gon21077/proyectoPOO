d116c11e8fa39e5159638578d5eb3534fb5300ae
sebastianf232 <fra21484@uvg.edu.gt>
2021-10-11 21:32:29 -0600

    Creación de clase Puntaje

---
+++ Puntaje.java
@@ -0,0 +1,32 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Proyecto Final POO
+ * File: Puntaje
+ * @author Diana Díaz 21066
+ * @author Sebastián Franco 21484
+ * @author Juan Miguel González-Campo 21077
+ * @author Mario Puente 21290
+ * @author Sebastián Reyes 21239
+ */
+public class Puntaje extends preguntas{
+    private int Respuesta;
+    private int puntos;
+
+    public Puntaje(){
+        super();
+    }
+
+    /**
+     * @param puntos
+     */
+    public void setPuntaje(int puntos){
+
+    }
+
+    /**
+     * @return puntos
+     */
+    public int getPuntaje(){
+        return puntos;
+    }
+}

6353bf4297ab05dab198f37820b3505dbdaf4183
dianadiaz02 <80539135+dianadiaz02@users.noreply.github.com>
2021-10-15 15:20:31 -0600

    Update Archivos.java

--- Archivos.java
+++ Archivos.java
@@ -9,12 +9,14 @@
  * @author Sebastián Reyes 21239
  */
 
+/*Esta es la clase de archivos, aqui están los ll¡inks para tutoriales*/
 public class Archivos {
     private String link_suma;
     private String link_resta;
     private String link_multiplicacion;
     private String link_division;
 
+    /*Este es el constructor*/
     public Archivos() {
         link_suma = "https://www.youtube.com/watch?v=oF-rZLIShC8";
         link_resta = "https://www.youtube.com/watch?v=Lqf5WmulMYI";
@@ -22,34 +24,66 @@ public class Archivos {
         link_division = "https://www.youtube.com/watch?v=mQ4wKV9_pZs";
     }
 
+    /*
+    * es el get suma
+    * @return link_suma
+    */
     public String getLink_suma() {
         return link_suma;
     }
 
+    /*
+    * es el get del link para respueta
+    * @return link_resta
+    */
     public String getLink_resta() {
         return link_resta;
     }
 
+    /*
+    * es el get para el link de multiplicacion
+    * @return link_multiplicacion
+    */
     public String getLink_multiplicacion() {
         return link_multiplicacion;
     }
 
+    /*
+    *es el get para el link de la division
+    * @return link_division
+    */
     public String getLink_division() {
         return link_division;
     }
 
+    /*
+    * es el set para el link de suma
+    * @param link_suma
+    */
     public void setLink_suma(String link_suma) {
         this.link_suma = link_suma;
     }
 
+    /*
+    * es el set para el link de la resta
+    *@param link_resta
+    */
     public void setLink_resta(String link_resta) {
         this.link_resta = link_resta;
     }
 
+    /*
+    * es el set para el link de la multipicacion
+    *@param link_multiplicacion
+    */
     public void setLink_multiplicacion(String link_multiplicacion) {
         this.link_multiplicacion = link_multiplicacion;
     }
 
+    /*
+    *es el set para el link de la division
+    *@param link_divison
+    */
     public void setLink_division(String link_division) {
         this.link_division = link_division;
     }

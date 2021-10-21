c66f8e6c5d1a639150ff635c75c96c866814abaa
sebasryes <83566772+sebasryes@users.noreply.github.com>
2021-10-20 23:37:38 -0600

    Cambio en toda la clase Archivos

--- Archivos.java
+++ Archivos.java
@@ -9,82 +9,78 @@
  * @author Sebastián Reyes 21239
  */
 
+import java.util.Scanner;
+
+import javax.swing.*;
+
+import java.awt.*;
+
 /*Esta es la clase de archivos, aqui están los ll¡inks para tutoriales*/
 public class Archivos {
-    private String link_suma;
-    private String link_resta;
-    private String link_multiplicacion;
-    private String link_division;
+   
+    JPanel panel = new JPanel();
+   
+    public void Archivos(){
+        
+        Titulo();
+        linksuma();
+        linkresta();
+        linkmult();
+        linkdiv();
+        
+        JFrame marco = new JFrame();
+        panel.setLayout(null);
+        panel.setBackground(Color.BLUE);
+        marco.add(panel);
+        marco.setTitle("Archivos del juego");
+        marco.setSize(500, 500);
+        marco.setResizable(true);
+        marco.setLocationRelativeTo(null);
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marco.setVisible(true);
 
-    /*Este es el constructor*/
-    public Archivos() {
-        link_suma = "https://www.youtube.com/watch?v=oF-rZLIShC8";
-        link_resta = "https://www.youtube.com/watch?v=Lqf5WmulMYI";
-        link_multiplicacion = "https://www.youtube.com/watch?v=YFtEaVw5k1A";
-        link_division = "https://www.youtube.com/watch?v=mQ4wKV9_pZs";
     }
 
-    /*
-    * es el get suma
-    * @return link_suma
-    */
-    public String getLink_suma() {
-        return link_suma;
+    public void Titulo(){
+        JLabel titulo2 = new JLabel("Archivos del Juego");
+        titulo2.setForeground(Color.WHITE);
+        titulo2.setBounds(5, 0, 300, 80);
+        titulo2.setFont(new Font("cooper black",3,20));
+        panel.add(titulo2);
     }
 
-    /*
-    * es el get del link para respueta
-    * @return link_resta
-    */
-    public String getLink_resta() {
-        return link_resta;
+    public void linksuma(){
+        JLabel titulo3 = new JLabel("Video de Suma: " + " https://www.youtube.com/watch?v=oF-rZLIShC8");
+        titulo3.setForeground(Color.WHITE);
+        titulo3.setBounds(5, 30, 500, 80);
+        titulo3.setFont(new Font("cooper black",3,13));
+        panel.add(titulo3);
     }
 
-    /*
-    * es el get para el link de multiplicacion
-    * @return link_multiplicacion
-    */
-    public String getLink_multiplicacion() {
-        return link_multiplicacion;
+    public void linkresta(){
+        JLabel titulo3 = new JLabel("Video de Resta: " + " https://www.youtube.com/watch?v=Lqf5WmulMYI");
+        titulo3.setForeground(Color.WHITE);
+        titulo3.setBounds(5, 60, 500, 80);
+        titulo3.setFont(new Font("cooper black",3,13));
+        panel.add(titulo3);
     }
 
-    /*
-    *es el get para el link de la division
-    * @return link_division
-    */
-    public String getLink_division() {
-        return link_division;
+    public void linkmult(){
+        JLabel titulo3 = new JLabel("Video de Multiplicación: " + " https://www.youtube.com/watch?v=YFtEaVw5k1A");
+        titulo3.setForeground(Color.WHITE);
+        titulo3.setBounds(5, 90, 500, 80);
+        titulo3.setFont(new Font("cooper black",3,13));
+        panel.add(titulo3);
     }
 
-    /*
-    * es el set para el link de suma
-    * @param link_suma
-    */
-    public void setLink_suma(String link_suma) {
-        this.link_suma = link_suma;
+    public void linkdiv(){
+        JLabel titulo3 = new JLabel("Video de División: " + " https://www.youtube.com/watch?v=mQ4wKV9_pZs");
+        titulo3.setForeground(Color.WHITE);
+        titulo3.setBounds(5, 120, 500, 80);
+        titulo3.setFont(new Font("cooper black",3,13));
+        panel.add(titulo3);
     }
 
-    /*
-    * es el set para el link de la resta
-    *@param link_resta
-    */
-    public void setLink_resta(String link_resta) {
-        this.link_resta = link_resta;
-    }
 
-    /*
-    * es el set para el link de la multipicacion
-    *@param link_multiplicacion
-    */
-    public void setLink_multiplicacion(String link_multiplicacion) {
-        this.link_multiplicacion = link_multiplicacion;
+    
     }
-
-    /*
-    *es el set para el link de la division
-    *@param link_divison
-    */
-    public void setLink_division(String link_division) {
-        this.link_division = link_division;
-    }
-}
